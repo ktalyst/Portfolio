87e5d03c422c7226dd192d5fcce1d42960789b5c
ktalyst <jnroudet@gmail.com>
2016-06-05 23:23:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="bower_components/Materialize/dist/css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+    <nav class="white" role="navigation">
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">Portfolio</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Compétences</a></li>
+                <li><a href="#">Formations</a></li>
+                <li><a href="#">Expérience</a></li>
+                <li><a href="#">Calendrier</a></li>
+                <li><a href="#">Me contacter</a></li>
+            </ul>
+
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Compétences</a></li>
+                <li><a href="#">Formations</a></li>
+                <li><a href="#">Expérience</a></li>
+                <li><a href="#">Calendrier</a></li>
+                <li><a href="#">Me contacter</a></li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+
+    <div id="index-banner" class="parallax-container">
+        <div class="section no-pad-bot">
+            <div class="container">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+          <!--   About Section   -->
+        <section class="about">
+            <div class="row">
+                <div class="profile">
+                    <div class="col s7 offset-s5">
+                        <div class="profile-info">
+                            <h4 class="profile-title">Je suis<span class="teal-text"> Jennifer Buisson</span></h4>
+                            <h5 class="profile-job">Développeuse</h5></div>
+                            <ul class="profile-list">
+                                <li class="clearfix">
+                                    <strong class="title">Age</strong>
+                                    <span class="text-right">26</span>
+                                </li>
+                                <li class="clearfix">
+                                    <strong class="title">Adresse</strong>
+                                    <span class="text-right">Lotissements les Crets, Massongy</span>
+                                </li>
+                                <li class="clearfix">
+                                    <strong class="title">E-mail</strong>
+                                    <span class="text-right"><a href="mailto:jnroudet@gmail.com">jnroudet@gmail.com</a></span>
+                                </li>
+                                <li class="clearfix">
+                                    <strong class="title">Téléphone</strong>
+                                    <span class="text-right"><a href="tel:+33652343557">+33 6 52 34 35 57</a></span>
+                                </li>
+                                <li class="clearfix">
+                                    <strong class="title">Situation</strong>
+                                    <span class="text-right">Célibataire</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="row">
+            <div class="profile-social">
+                <ul class="social">
+                    <li><a class="ripple-centered" href="https://twitter.com" target="_blank"><i class="zmdi zmdi-twitter zmdi-hc-lg"></i></a></li>
+                    <li><a class="ripple-centered" href="https://www.linkedin.com" target="_blank"><i class="zmdi zmdi-twitter zmdi-hc-lg"></i></a></li>
+                    <li><a class="ripple-centered" href="https://www.viadeo.com" target="_blank"><i class="icon icon-viadeo"></i></a></li>
+                    <li><a class="ripple-centered" href="https://www.flikr.com" target="_blank"><i class="icon icon-instagram"></i></a></li>
+                    <li><a class="ripple-centered" href="https://www.github.com" target="_blank"><i class="icon icon-github"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="bower_components/Materialize/dist/js/materialize.min.js"></script>
+  </body>
+</html>
